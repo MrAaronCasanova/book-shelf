e8cea3174dffabb58f0d16064a528cef9405794a
MrAaronCasanova <1boris231@gmail.com>
2018-04-20 12:35:08 -0700

    full crud setup on book schema

--- server/server.js
+++ server/server.js
@@ -52,8 +52,24 @@ app.post('/api/book', (req, res) => {
 });
 
 // UPDATE //
+app.post('/api/book_update', (req, res) => {
+  Book.findByIdAndUpdate(req.body._id, req.body, { new: true }, (err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.json({
+      success: true,
+      doc
+    });
+  });
+});
 
 // DELETE //
+app.delete('/api/delete_book', (req, res) => {
+  let id = req.query.id;
+  Book.findByIdAndRemove(id, (err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.json(true);
+  });
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
